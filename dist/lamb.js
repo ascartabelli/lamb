0d992c664ac0563f9bcff90a3d46785ad969af1b
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2016-07-07 09:34:49 +0200

    updated dev dependencies

--- dist/lamb.js
+++ dist/lamb.js
@@ -1,7 +1,7 @@
 /**
  * @overview lamb - A lightweight, and docile, JavaScript library to help embracing functional programming.
  * @author Andrea Scartabelli <andrea.scartabelli@gmail.com>
- * @version 0.33.0-alpha.5
+ * @version 0.33.0-alpha.6
  * @module lamb
  * @license MIT
  * @preserve
@@ -18,7 +18,7 @@
      * @category Core
      * @type String
      */
-    lamb._version =  "0.33.0-alpha.5";
+    lamb._version =  "0.33.0-alpha.6";
 
     // alias used as a placeholder argument for partial application
     var _ = lamb;
